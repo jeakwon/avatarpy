fb63a62478c235dd46af3da518db2c15fdc89586
jeakwon <onlytojay@gmail.com>
2021-04-21 14:06:27 +0900

    minor change

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from avatarpy import Avatar
 
 avatar = Avatar(r"C:\Users\Jay\Downloads\3Dpose_IBS\D1_Hab_01.mp4.txt.csv")
-avatar
-# avatar = avatar.transform.align_on_axis()
-# avatar.animate.make(avatar.index[:10]).show()
+avatar.help
